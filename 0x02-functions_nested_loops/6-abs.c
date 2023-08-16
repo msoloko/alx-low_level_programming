22bf641e9458d737237d3f61864cf65c29b5ae5c
msoloko <msoloko@gmail.com>
2023-08-15 21:15:44 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,4 +1,4 @@
-#include "main.c"
+#include "main.h"
 /**
  * _abs - computes the absolute value of an integer
  * @n: the int to check
@@ -6,9 +6,12 @@
  */
 int _abs(int n)
 {
-	if (n >= 0)
+	if (n < 0)
 	{
-		return (n);
+		int abs_val;
+
+		abs_val = n * -1;
+		return (abs_val);
 	}
-	return (-n);
+	return (n);
 }
