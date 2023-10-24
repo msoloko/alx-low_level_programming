4cd6b336d4dbc8315124aea79a164b5c249e0cac
msoloko <msoloko@gmail.com>
2023-10-24 02:27:22 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,15 +1,14 @@
 #include <stdlib.h>
-	#include "dog.h"
+#include "dog.h"
 
-
-	/**
-	 * _strlen - returns the length of a string
-	 * @s: string to evaluate
-	 *
-	 * Return: the length of the string
-	 */
-	int _strlen(char *s)
-	{
+/**
+* _strlen - returns the length of a string
+* @s: string to evaluate
+*
+* Return: the length of the string
+*/
+int _strlen(char *s)
+{
 		int i;
 
 
@@ -23,20 +22,19 @@
 
 
 		return (i);
-	}
+}
 
-
-	/**
-	 * *_strcpy - copies the string pointed to by src
-	 * including the terminating null byte (\0)
-	 * to the buffer pointed to by dest
-	 * @dest: pointer to the buffer in which we copy the string
-	 * @src: string to be copied
-	 *
-	 * Return: the pointer to dest
-	 */
-	char *_strcpy(char *dest, char *src)
-	{
+/**
+* *_strcpy - copies the string pointed to by src
+* including the terminating null byte (\0)
+* to the buffer pointed to by dest
+* @dest: pointer to the buffer in which we copy the string
+* @src: string to be copied
+*
+* Return: the pointer to dest
+*/
+char *_strcpy(char *dest, char *src)
+{
 		int len, i;
 
 
@@ -52,23 +50,20 @@
 		for (i = 0; i < len; i++)
 		{
 			dest[i] = src[i];
-		}
-		dest[i] = '\0';
+		}		dest[i] = '\0';
 
+	return (dest);
+}
 
-		return (dest);
-	}
-
-
-	/**
-	 * new_dog - creates a new dog
-	 * @name: name of the dog
-	 * @age: age of the dog
-	 * @owner: owner of the dog
-	 *
-	 * Return: pointer to the new dog (Success), NULL otherwise
-	 */
-	dog_t *new_dog(char *name, float age, char *owner)
+/**
+* new_dog - creates a new dog
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+*
+* Return: pointer to the new dog (Success), NULL otherwise
+*/
+dog_t *new_dog(char *name, float age, char *owner)
 	{
 		dog_t *dog;
 		int len1, len2;
@@ -102,4 +97,4 @@
 
 
 		return (dog);
-	}
+}
